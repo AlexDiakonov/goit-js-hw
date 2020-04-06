6a4ca3c4727dd5aad9b94c74f13a7dee5ffb3c2b
AlexDiakonov <imalex.diakonov@gmail.com>
2020-04-06 20:22:15 +0300

    Module -5 isDone

---
+++ homework-5/task3.js
@@ -0,0 +1,31 @@
+"use strict";
+
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(...itemsToAdd) {
+    this.items.push(...itemsToAdd);
+  }
+  removeItem(removedItem) {
+    this.items.splice(this.items.indexOf(removedItem), 1);
+  }
+}
+
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор"
+]);
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem("Дроид");
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
