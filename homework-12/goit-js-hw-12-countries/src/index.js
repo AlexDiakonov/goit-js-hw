8827d87244a41df4234507e456399ae06536620d
AlexDiakonov <imalex.diakonov@gmail.com>
2020-07-01 17:58:27 +0300

    lets do it

---
+++ homework-12/goit-js-hw-12-countries/src/index.js
@@ -0,0 +1,10 @@
+// import template from "./template/menu.hbs";
+import "./styles.css";
+import "./request";
+import "./fetchCountries";
+
+// buildMenu(menu);
+// function buildMenu(menu) {
+//   const markUp = menu.map((menuItem) => template(menuItem)).join("");
+//   menuList.insertAdjacentHTML("beforeend", markUp);
+// }
